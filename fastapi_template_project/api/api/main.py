80b141294a681de4efd0da23a8de023909183bce
gabrielbazan <gbazan@outlook.com>
2022-06-10 15:32:12 -0300

    Enhance main.py

--- fastapi_template_project/api/api/main.py
+++ fastapi_template_project/api/api/main.py
@@ -4,19 +4,14 @@ from routers import ALL_ROUTERS
 from cors import configure_cors
 
 
-def create_app():
-    fast_api_app = FastAPI()
-    configure_cors(fast_api_app)
-    include_routers(fast_api_app, ALL_ROUTERS)
-    return fast_api_app
-
-
 def include_routers(app: FastAPI, routers: List[APIRouter]) -> None:
     for router in routers:
         app.include_router(router)
 
 
-app = create_app()
+app = FastAPI()
+configure_cors(app)
+include_routers(app, ALL_ROUTERS)
 
 
 @app.get("/")
