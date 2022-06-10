3d933b825684bfb7c0a43b872af8e0f311a06d4a
gabrielbazan <gbazan@outlook.com>
2022-06-10 17:27:19 -0300

    Install requirements in venv when creating it

--- create_venv.sh
+++ create_venv.sh
@@ -1,10 +1,14 @@
 
 
 VIRTUALENV_PATH="venv"
+REQUIREMENTS_FILE_PATH=fastapi_template_project/api/api/requirements.frozen
 
 
 echo "Creating virtualenv..."
 python3 -m venv ${VIRTUALENV_PATH}
+
+echo "Installing requirements..."
+${VIRTUALENV_PATH}/bin/pip install -r ${REQUIREMENTS_FILE_PATH}
 
 
 echo "Done!"
